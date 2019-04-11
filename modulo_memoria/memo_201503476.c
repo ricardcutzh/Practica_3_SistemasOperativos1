99b80eb0aea54618a3dddbf45eae83a9eaf6c183
ricardcutzh <ricardcutzh@gmail.com>
2019-04-11 00:49:07 -0600

    itoa

--- modulo_memoria/memo_201503476.c
+++ modulo_memoria/memo_201503476.c
@@ -40,6 +40,32 @@ static struct file_operations fops; //OPERACIONES CON EL ARCHIVO
 static char *message; //MENSAJE A DESPLEGAR EN EL PROC
 static int read_p;
 
+//FUNCION AUXILIAR ITOA
+char *itoa (unsigned long value, char *result, int base)
+{
+    // check that the base if valid
+    if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+    char* ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
+
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+    } while ( value );
+
+    // Apply negative sign
+    if (tmp_value < 0) *ptr++ = '-';
+    *ptr-- = '\0';
+    while (ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr--= *ptr1;
+        *ptr1++ = tmp_char;
+    }
+    return result;
+}
+
 //CREACION
 int mod_memoria_proc_open(struct inode *sp_inode, struct file *sp_file)
 {
@@ -85,31 +111,7 @@ int mod_memoria_proc_release(struct inode *sp_inode, struct file *sp_file)
     return 0;
 }
 
-//FUNCION AUXILIAR ITOA
-char *itoa (unsigned long value, char *result, int base)
-{
-    // check that the base if valid
-    if (base < 2 || base > 36) { *result = '\0'; return result; }
 
-    char* ptr = result, *ptr1 = result, tmp_char;
-    int tmp_value;
-
-    do {
-        tmp_value = value;
-        value /= base;
-        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-    } while ( value );
-
-    // Apply negative sign
-    if (tmp_value < 0) *ptr++ = '-';
-    *ptr-- = '\0';
-    while (ptr1 < ptr) {
-        tmp_char = *ptr;
-        *ptr--= *ptr1;
-        *ptr1++ = tmp_char;
-    }
-    return result;
-}
 
 //FUNCION DE INICIO
 static int memoria_init(void)
